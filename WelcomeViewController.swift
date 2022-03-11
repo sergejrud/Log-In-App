083cb442481148377f34d72734145c6732d29e9f
Sergei Rud <sergejrud@gmail.com>
2022-03-11 17:42:33 +0300

    View Controller settings

--- WelcomeViewController.swift
+++ WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Log-In App
 //
 //  Created by Сергей Рудь on 11.03.22.
@@ -7,15 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    private var username = "User"
-    private var password = "Password"
-    
+class WelcomeViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
 
 }
-
